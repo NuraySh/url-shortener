3b5d193e1bfbbaa38624c516d6daba2c8419b311
Apple Macbook Air M1 <applemacbookairm1@Apples-MacBook-Air.local>
2022-09-13 11:11:14 +0400

    done

---
+++ shortener/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+import uuid
+from .models import Url
+# Create your views here.
+
+def index(request):
+    return render(request, 'main.html')
+
+
+def create(request):
+    if request.method == "POST":
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+    return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    if url_details.link.startswith('https://'):
+        return redirect(url_details.link)
+    else:
+        return redirect("https://" + url_details.link)
+    
